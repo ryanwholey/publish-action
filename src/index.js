6776054b9a9289427227400e791e455c7894119d
Ryan Wholey <ryan@takescoop.com>
2021-03-01 17:23:27 -0800

    log more

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ const axios = require('axios')
 const fs = require('fs')
 
 async function loadGithubEvent(path) {
-  return await fs.promises.readFile(path)
+  return JSON.parse(await fs.promises.readFile(path))
 }
 
 ;(async () => {
+  const sonarUrl = process.env.INPUT_SONAR_URL
   const event = await loadGithubEvent(process.env.GITHUB_EVENT_PATH)
 
-  console.log(event.comment)
+  console.log(sonarUrl)
   console.log(process.env)
+  console.log(event)
 })()
